1f15bfbb1f05a825bc61dbafecc43ca323075e05
voyula <40173603+voyula@users.noreply.github.com>
2018-09-30 23:54:57 +0300

    Update MethodChainingTest.php

--- Other/MethodChaining/Tests/MethodChainingTest.php
+++ Other/MethodChaining/Tests/MethodChainingTest.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace DesignPatterns\Other\MethodChaining\Tests;
+
+use PHPUnit\Framework\TestCase;
+use DesignPatterns\Other\MethodChaining\MethodChaining;
+
+class MethodChainingTest extends TestCase
+{
+    /**
+     * @var \DesignPatterns\Other\MethodChaining\MethodChaining
+     */
+    private methodChaining;
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->methodChaining = new MethodChaining;
+    }
+    /**
+     * @return void
+     */
+    public function testHello(): void
+    {
+        $this->methodChaining->methodOne()->methodTwo()->methodThree()->methodFour()->methodFive();
+        $this->assertEquals('Hello', $this->methodChaining);
+    }
+}
