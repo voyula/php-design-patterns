405f8fbbb85be17255019d875c7d6f0730a50f86
voyula <40173603+voyula@users.noreply.github.com>
2018-09-30 23:35:54 +0300

    Create MethodChaining.php

---
+++ Other/MethodChaining.php
@@ -0,0 +1,36 @@
+<?php
+
+class MethodChaining
+{
+    public $hello;
+
+    public function method1()
+    {
+        $hello .= 'H';
+        return $this;
+    }
+
+    public function method2()
+    {
+        $hello .= 'e';
+        return $this;
+    }
+
+    public function method3()
+    {
+        $hello .= 'l';
+        return $this;
+    }
+
+    public function method4()
+    {
+        $hello .= 'l';
+        return $this;
+    }
+
+    public function method5()
+    {
+        $hello .= 'o';
+        return $this;
+    }
+}
