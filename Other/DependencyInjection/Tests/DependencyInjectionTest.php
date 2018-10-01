1790798b9ba279b5bd0879446d9f743f063dba12
voyula <40173603+voyula@users.noreply.github.com>
2018-10-01 13:15:21 +0300

    Create DependencyInjectionTest.php

---
+++ Other/DependencyInjection/Tests/DependencyInjectionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DesignPatterns\Other\DependencyInjection\Tests;
+
+use PHPUnit\Framework\TestCase;
+use DesignPatterns\Other\DependencyInjection\DependencyInjection;
+
+class DependencyInjectionTest extends TestCase
+{
+    /**
+     * @var \DesignPatterns\Other\DependencyInjection\DependencyInjection
+     */
+    private dependencyInjection;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->dependencyInjection = new DependencyInjection(new DateTime(null, new DateTimeZone('Europe/London')));
+    }
+
+    /**
+     * @return void
+     */
+    public function testDependencyInjection(): void
+    {
+        $this->assertEquals('Europe/London', $this->dependencyInjection->getTimezoneName());
+    }
+}
