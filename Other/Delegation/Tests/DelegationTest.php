f6ad37bb84c718c083b989b039f3af2a0859a9a8
User <user@mail.com>
2018-12-20 00:47:25 +0300

    Added Delegation Pattern

---
+++ Other/Delegation/Tests/DelegationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DesignPatterns\Other\Delegation\Tests;
+
+use PHPUnit\Framework\TestCase;
+use DesignPatterns\Other\Delegation\Delegator;
+
+class DelegationTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testDelegator(): void
+    {
+        $delegator = new Delegator;
+        $this->assertEquals('Mail sent: example@example.com', $delegator->send());
+    }
+}
